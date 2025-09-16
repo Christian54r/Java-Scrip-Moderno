ffd516de5e400900a7696def4832fcd141c989d5
Christian54r <cris38421@hotmail.com>
2025-09-15 22:54:53 -0600

    CAMBIOS INTRODUCCION

--- Curso JS Moderno/01-Introduccion/app.js
+++ Curso JS Moderno/01-Introduccion/app.js
@@ -2,3 +2,20 @@
 const nombre = prompt("Cual es tu nombre?");
 //Toma el nombre como variable ingresada y lo muestra en pantalla
 document.querySelector(".contenido").innerHTML = "${nombre} esta aprendiendo JavaScrip moderno";
+
+//Crea una variable llamada hola para comenzar a medir el tiempo
+console.time(`Hola`);
+//Se ejecutan mensajes de alerta para medir el tiempo
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+console.warn(`Eso no esta permitido`);
+
+//Termina de medir el tiempo mediante la ejecucion de las alertas
+console.timeEnd(`Hola`);
