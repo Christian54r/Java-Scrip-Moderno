9a556b564ed9923d61c9f3a14637a70435907646
Christian54r <cris38421@hotmail.com>
2025-10-17 12:54:40 -0600

    Cambios en Strings app03

--- Curso JS Moderno/03-Strings/js/03-app.js
+++ Curso JS Moderno/03-Strings/js/03-app.js
@@ -1,18 +1,55 @@
-//variables creadas
-const producto = " Monitor 20 pulgadas";
-const precio = " 30 USD";
+const producto = "Macbook Pro 17 Pulgadas";
+const precio = " 1,300 USD ";
+const texto1 = "Concatenar variables";
+const texto2 = "Concatenar strings";
+const texto3 = "Concatenar con signo de +";
+const texto4 = "Concatenar con comas(,) y strings";
+const texto5 = "Concatenar con el signo ${} entre comilla de (') y strings"
 
-//concatenar con variables
-console.log(producto.concat(precio));
 
-//concatenar con string
-console.log(producto.concat(" En descuento"))
+console.log(texto1)
+console.log(producto.concat(precio))
 
-// Concatenar con signo de + tambien se pueden agregar string
-console.log( producto + " tiene un precio de: " + precio);
+console.log(texto2)
+console.log(producto.concat(" tiene un precio de ").concat(precio))
 
-// Concatenar con comas , y strings
-console.log( producto," tiene un precio de:",precio);
+console.log(texto3)
+console.log( producto + " tiene un valor de " + precio)
 
-//Concatenar  con strings y con el signo ${} entre comilla de ``
-console.log(`El producto ${producto} tiene un precio $ ${precio}`);
+console.log(texto4)
+console.log(producto, "Cuesta:",precio)
+
+console.log(texto5)
+console.log(`El producto ${producto} tiene un precio de ${precio}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //variables creadas
+// const producto = " Monitor 20 pulgadas";
+// const precio = " 30 USD";
+
+// //concatenar con variables
+// console.log(producto.concat(precio));
+
+// //concatenar con string
+// console.log(producto.concat(" En descuento"))
+
+// // Concatenar con signo de + tambien se pueden agregar string
+// console.log( producto + " tiene un precio de: " + precio);
+
+// // Concatenar con comas , y strings
+// console.log( producto," tiene un precio de:",precio);
+
+// //Concatenar  con strings y con el signo ${} entre comilla de ``
+// console.log(`El producto ${producto} tiene un precio $ ${precio}`);
