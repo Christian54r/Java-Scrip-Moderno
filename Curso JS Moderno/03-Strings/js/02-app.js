55f0f2ca932de41574d0d5ee7393c75bc5af2049
Christian54r <cris38421@hotmail.com>
2025-10-16 19:39:45 -0600

    Cambiis en String Archivo app 2

--- Curso JS Moderno/03-Strings/js/02-app.js
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -1,16 +1,48 @@
-const producto = "Monitor 20 Pulgadas";
+const producto1 = "Monitor 30 Pulgadas";
+const producto2 = "Nueva Tablet Lenovo";
+const letras1 = "Medir cantidad de letras de texto";
+const texto1 = "Verifica en cual numero de linea comienza el texto Tablet";
+const texto2 = "Verifica si la palabra Tablet esta dentro de la constante indicada en este caso producto2";
+const texto3 = "Verifica si la palabra Tablet esta dentro de la constante indicada en este caso producto1"
 
-console.log(producto)
+console.log(producto1)
 
-//Conocer la cantidad de letras de la cadena de texto
-console.log(producto.length);
+console.log(letras1)
 
-//Busca por numero de linea un nombre
-// console.log(producto.indexOf("Tablet"))
+console.log(producto1.length)
+console.log(texto1)
+console.log(producto2.indexOf("Tablet"))
+console.log(texto2)
+console.log(producto2.includes("Tablet"))
+console.log(texto3)
+console.log(producto1.includes("Tablet"))
 
-//Busca por nombre el producto
-console.log(producto.includes("Tablet"))
-console.log(producto.includes("Monitor"))
-//Si es escribre alguna letra diferente por ejemplo mayuscula y minuscula no va encontrar el nombre tiene que ser la misma escritura
-console.log(producto.includes("monitor"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const producto = "Monitor 20 Pulgadas";
+
+// console.log(producto)
+
+// //Conocer la cantidad de letras de la cadena de texto
+// console.log(producto.length);
+
+// //Busca por numero de linea un nombre
+// // console.log(producto.indexOf("Tablet"))
+
+// //Busca por nombre el producto
+// console.log(producto.includes("Tablet"))
+// console.log(producto.includes("Monitor"))
+// //Si es escribre alguna letra diferente por ejemplo mayuscula y minuscula no va encontrar el nombre tiene que ser la misma escritura
+// console.log(producto.includes("monitor"))
 
