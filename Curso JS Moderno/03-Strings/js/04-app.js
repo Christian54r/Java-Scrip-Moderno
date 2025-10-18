94332128426733f077cd39a26292835784e6e173
Christian54r <cris38421@hotmail.com>
2025-10-17 19:18:08 -0600

    Cambios en Strings App04

--- Curso JS Moderno/03-Strings/js/04-app.js
+++ Curso JS Moderno/03-Strings/js/04-app.js
@@ -1,18 +1,57 @@
-const producto = "               Monitor 20 Pulgadas         "
-
-console.log(producto)
-console.log(producto.length)
-
-
-//Eliminar espacios del inicio
-console.log(producto.trimStart());
+const producto = "    Monitor 20 Pulgadas    ";
+const texto1 = "Medimos cuantos caracteres son contando los espacios";
+const texto2 = "Eliminamos espacios del inicio y medimos cuantos caracteres son sin los espacios del inicio";
+const texto3 = "Eliminamos espacios del final y medimos cuantos caracteres son sin los espacios del final";
+const texto4 = "Eliminamos los espacios elijiendo la direccion"
+const texto5 = "Eliminamos los espacios en ambas direciones"
 
 
-//Eliminar espacios al final
-console.log(producto.trimEnd());
 
-//Eliminar espacios de inicio y final elijiendo la direccion
-console.log(producto.trimStart().trimEnd())
+console.log(producto);
+console.log(texto1);
+console.log(producto.length);
 
-//Eliminar espacios para ambas direcciones
+console.log(texto2);
+console.log(producto.trimStart().length);
+
+console.log(texto3);
+console.log(producto.trimEnd().length);
+
+console.log(texto4);
+console.log(producto.trimStart().trimEnd());
+
+console.log(texto5);
 console.log(producto.trim());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const producto = "               Monitor 20 Pulgadas         "
+
+// console.log(producto)
+// console.log(producto.length)
+
+
+// //Eliminar espacios del inicio
+// console.log(producto.trimStart());
+
+
+// //Eliminar espacios al final
+// console.log(producto.trimEnd());
+
+// //Eliminar espacios de inicio y final elijiendo la direccion
+// console.log(producto.trimStart().trimEnd())
+
+// //Eliminar espacios para ambas direcciones
+// console.log(producto.trim());
