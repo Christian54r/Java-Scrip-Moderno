0ea17b3f4c06a24f59957678a27555c64eb7d8cd
Christian54r <Cris38421@hotmail.com>
2025-10-25 18:54:46 -0600

    cambios String App-06

--- Curso JS Moderno/03-Strings/js/06-app.js
+++ Curso JS Moderno/03-Strings/js/06-app.js
@@ -9,10 +9,13 @@ console.log(`${producto} ${texto} !!!`);
 
 // Split nos permite dividir un string
 const actividad = "Estoy aprendiendo JavaScrip Moderno";
+//divide los strings porque se selecionan los espacios para que se coloquen las comas
 console.log(actividad.split(" "))
 
+//divide los strings porque se selecionan las comas para que se coloquen las comas
 const hobbies = "Leer,caminar,escuchar,musica,escribir,aprender a programar";
 console.log(hobbies.split(","));
 
+//divide los strings porque se seleciona el # para que se coloquen las comas
 const tweet = "Aprendiendo JavaScript #JSModernoConJuan";
 console.log(tweet.split("#"));
